06d11e296c1a6436851be9c14bb42a6a6a00d5bd
Oana Donose <oana.donose@mmtdigital.co.uk>
2019-08-29 11:44:01 +0100

    made sound play only when site goes down

--- src/App.js
+++ src/App.js
@@ -10,26 +10,25 @@ class App extends Component {
     this.state = {
       monitorsArray: [],
       key: 'u630058-e28e0007af00faaf27512f11',
+      downMonitorsArray: []
     }
     this.popSound = new Audio('Bubble-pop-sound-effect.mp3');
   }
-
-  
-async loadData() {
-  console.log("loading data...");
-  try {
-    var options = {
-      credentials: "omit",
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        "accept":"application/json",
-        "content-type":"application/json",
-        "sec-fetch-mode":"cors"
-      },
-      body: 
-      `{"api_key":"u630058-e28e0007af00faaf27512f11","all_time_uptime_ratio":1,"response_times_average":1}`
-  };
+  async loadData() {
+    console.log("loading data...");
+    try {
+      var options = {
+        credentials: "omit",
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          "accept":"application/json",
+          "content-type":"application/json",
+          "sec-fetch-mode":"cors"
+        },
+        body: 
+        `{"api_key":"u630058-e28e0007af00faaf27512f11","all_time_uptime_ratio":1,"response_times_average":1}`
+    };
 
   fetch(`https://api.uptimerobot.com/v2/getMonitors?api_key=u630058-e28e0007af00faaf27512f11&format=json&logs=1`, options)
   .then(data => data.json())
@@ -51,14 +50,28 @@ componentDidMount() {
   this.loadData();
   setInterval(() => this.loadData(), 10000);
 }
-  componentDidUpdate(){
-    this.state.monitorsArray.forEach(monitor=> {
-      if (monitor.status === 0) {
+componentDidUpdate(){
+  this.state.monitorsArray.forEach(monitor => {
+    if(monitor.status === 0) {
+      if(this.state.downMonitorsArray.includes(monitor.friendly_name)) {
+        //do nothing? monitor is already in down array
+      }
+      else {
+        this.state.downMonitorsArray.push(monitor.friendly_name);
+        console.log("added monitor to down array.");
         this.popSound.play();
       }
-    });
-
-  }
+    }
+    else {
+      if(this.state.downMonitorsArray.includes(monitor.friendly_name)) {
+        //if down monitors array includes a monitor that now is not down
+        const toRemove = this.state.downMonitorsArray.indexOf(monitor.friendly_name);
+        this.state.downMonitorsArray.splice(toRemove,1); //remove from the down monitors 
+        console.log("after removing monitor that is no longer down: " + this.state.downMonitorsArray);
+      }
+    }
+  })
+}
     render () {
       return (
         <div className="App">
